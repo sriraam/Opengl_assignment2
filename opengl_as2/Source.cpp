27d76b5111757512a1fec4c24d34f2d857a54861
Sri Raam <Sri Raam@LAPTOP-JEB3P6D8>
2017-02-24 20:32:08 +0530

    enum for different modes

--- opengl_as2/Source.cpp
+++ opengl_as2/Source.cpp
@@ -4,6 +4,16 @@
 GLfloat v[4][2];
 GLint  obj[5];
 int static count=0;
+int mode = 1;
+
+enum draw_mode {
+	line = 1,
+	rectangle,
+	polygon,
+	circle
+}md;
+
+
 bool handle=false;
 /*
 class ver_handle {
@@ -35,13 +45,10 @@ public:
 
 
 void click_handle(int button, int state, int x, int y) {
-	if (button == GLUT_LEFT_BUTTON && state==GLUT_DOWN) {
-
-
+	if (button == GLUT_LEFT_BUTTON && state==GLUT_UP) {
+		
 		//ver_handle *a;
 		//a = new ver_handle;
-
-		
 		v[count][0] = x;
 		v[count][1] = y;
 		//v[count+1][0] = x;
@@ -53,6 +60,19 @@ void click_handle(int button, int state, int x, int y) {
 	}
 }
 
+void menuhandler(int a) {
+	mode=a;
+}
+
+void menufunc() {
+	glutCreateMenu(menuhandler);
+	glutAddMenuEntry("LINE", md=line);
+	glutAddMenuEntry("RECTANGLE", md=rectangle);
+	glutAddMenuEntry("POLYGON", md=polygon);
+	glutAddMenuEntry("CIRCLE", md=circle);
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+}
+
 void init() {
 	glClearColor(1, 1, 1, 1);
 	gluOrtho2D(0, 640, 0, 480);
@@ -60,38 +80,46 @@ void init() {
 	glFlush();
 }
 
+void draw_ver() {
+	int i;
+	for (i = 0; i < count; i++) {
+		//ver_handle v;
+		//v.cross();
+
+		glPointSize(15);
+		glBegin(GL_POINTS);
+		glColor3f(0, 0, 1);
+		glVertex2f(v[i][0], 480 - v[i][1]);
+		glEnd();
+
+	}
+
+}
+
 void render() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	if (handle == true) {
 		int i;
-		
-		for (i = 0; i < count; i++) {
-			//ver_handle v;
-			//v.cross();
+		std::cout << "entered render";
+		std::cout << md;
+		if (md == 1) {
+			std::cout << "entered line";
+			draw_ver();
+			i = 0;
+			int temp;
+			temp = count - (count % 2);
 
-			glPointSize(15);
-			glBegin(GL_POINTS);
-			glColor3f(0, 0, 1);
-			glVertex2f(v[i][0], 480 - v[i][1]);
-			glEnd();
+			while (i < temp) {
 
-		}
-		
-				i = 0;
-				int temp;
-				temp = count - (count % 2);
-
-			while (i < temp ) {
-				
 				glBegin(GL_LINES);
 				std::cout << "came to line";
 				glColor3f(0, 0, 0);
 				glVertex2f(v[i][0], 480 - v[i][1]);
 				glVertex2f(v[i + 1][0], 480 - v[i + 1][1]);
 				glEnd();
-			i += 2;
+				i += 2;
 			}
-		
+		}
 				/*
 						std::cout << "line called";
 						glBegin(GL_LINES);
@@ -126,6 +154,7 @@ int main(int argc, char** argv) {
 	glutInitWindowSize(640, 480);
 	glutCreateWindow("DRAW PRI");
 	init();
+	menufunc();
 	glutDisplayFunc(render);
     glutMouseFunc(click_handle);
 	glutMainLoop();
