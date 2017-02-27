668386bc3d374174db6b7118170a5e0b316c994e
Sri Raam <Sri Raam@LAPTOP-JEB3P6D8>
2017-02-27 08:38:23 +0530

    perfect line &rect

--- opengl_as2/Source.cpp
+++ opengl_as2/Source.cpp
@@ -1,10 +1,42 @@
 #include<GL\glut.h>
 #include<iostream>
+#include<vector>
+
 
 GLfloat v[4][2];
 GLint  obj[5];
 int static count=0;
+int mode = 1;
+int j = 0;
 bool handle=false;
+
+std::vector<GLfloat> x_vec;
+std::vector<GLfloat> y_vec;
+std::vector<int> current_mode;
+
+
+//x_vec.reserve(10);
+
+enum draw_mode {
+	line = 1,
+	rectangle,
+	polygon,
+	circle
+}md;
+
+void menuhandler(int a) {
+	mode = a;
+	current_mode.push_back(mode);
+}
+
+void menufunc() {
+	glutCreateMenu(menuhandler);
+	glutAddMenuEntry("LINE", md = line);
+	glutAddMenuEntry("RECTANGLE", md = rectangle);
+	glutAddMenuEntry("POLYGON", md = polygon);
+	glutAddMenuEntry("CIRCLE", md = circle);
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+}
 /*
 class ver_handle {
 public:
@@ -38,16 +70,20 @@ void click_handle(int button, int state, int x, int y) {
 	if (button == GLUT_LEFT_BUTTON && state==GLUT_DOWN) {
 
 
-		//ver_handle *a;
-		//a = new ver_handle;
-
+		if (current_mode.size()==0) {
+			std::cout << "size =0";
+			current_mode.push_back(1);
+		}
 		
 		v[count][0] = x;
 		v[count][1] = y;
-		//v[count+1][0] = x;
-		//v[count+1][1] = y;
+
+		x_vec.push_back(x);
+		y_vec.push_back(y);
+		
+		
 		handle = true;
-		std::cout << "Mouse clicked";
+		//std::cout << "Mouse clicked";
 		count++;
 		
 	}
@@ -56,15 +92,49 @@ void click_handle(int button, int state, int x, int y) {
 void init() {
 	glClearColor(1, 1, 1, 1);
 	gluOrtho2D(0, 640, 0, 480);
-
+	x_vec.reserve(10);
+	y_vec.reserve(10);
+	current_mode.reserve(5);
 	glFlush();
+}
+
+void draw_line(int i_dp,int temp_dp) {
+	int i = i_dp;
+	int temp = temp_dp;
+	if (i < temp) {
+		std::cout << "in line\n";
+		glBegin(GL_LINES);
+		//std::cout << "came to line";
+		glColor3f(0, 0, 0);
+		//	glVertex2f(v[i][0], 480 - v[i][1]);
+		//  glVertex2f(v[i + 1][0], 480 - v[i + 1][1]);
+		glVertex2f(x_vec[i], 480 - y_vec[i]);
+		glVertex2f(x_vec[i + 1], 480 - y_vec[i + 1]);
+		glEnd();
+		
+	}
+}
+void draw_rect(int i_dp, int temp_dp) {
+	int i = i_dp;
+	int temp = temp_dp;
+	if (i < temp) {
+		glBegin(GL_LINE_LOOP);
+		std::cout << "in rec\n";
+		glColor3f(0, 1, 0);
+		glVertex2f(x_vec[i], 480 - y_vec[i]);
+		glVertex2f(x_vec[i], 480 - y_vec[i + 1]);
+		glVertex2f(x_vec[i + 1], 480 - y_vec[i + 1]);
+		glVertex2f(x_vec[i + 1], 480 - y_vec[i]);
+		glEnd();
+
+	}
 }
 
 void render() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	if (handle == true) {
 		int i;
-		
+		//int j;
 		for (i = 0; i < count; i++) {
 			//ver_handle v;
 			//v.cross();
@@ -72,26 +142,36 @@ void render() {
 			glPointSize(15);
 			glBegin(GL_POINTS);
 			glColor3f(0, 0, 1);
-			glVertex2f(v[i][0], 480 - v[i][1]);
+			//glVertex2f(v[i][0], 480 - v[i][1]);
+			glVertex2f(x_vec[i],480-y_vec[i]);
 			glEnd();
 
 		}
 		
 				i = 0;
-				int temp;
-				temp = count - (count % 2);
-
-			while (i < temp ) {
 				
-				glBegin(GL_LINES);
-				std::cout << "came to line";
-				glColor3f(0, 0, 0);
-				glVertex2f(v[i][0], 480 - v[i][1]);
-				glVertex2f(v[i + 1][0], 480 - v[i + 1][1]);
-				glEnd();
-			i += 2;
-			}
-		
+				int temp;
+				//temp = count - (count % 2);
+
+				std::cout << "\n";
+				for (int j = 0; j < current_mode.size(); j++) {
+
+					if (current_mode[j] == 1) {
+						temp = count - (count % 2);
+						std::cout << "j :" << j<<" val is"<< current_mode[j];
+						std::cout << "\nEntered line\n";
+						draw_line(i,temp);
+						i += 2;
+					}
+					else if (current_mode[j] == 2) {
+						temp = count - (count % 2);
+						std::cout << "j :" << j << " val is" << current_mode[j];
+						std::cout << "\nEntered rect\n";
+						draw_rect(i, temp);
+						i += 2;
+					}
+				}
+				
 				/*
 						std::cout << "line called";
 						glBegin(GL_LINES);
@@ -120,12 +200,14 @@ void render() {
 	glFlush();
 }
 
+
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(640, 480);
 	glutCreateWindow("DRAW PRI");
 	init();
+	menufunc();
 	glutDisplayFunc(render);
     glutMouseFunc(click_handle);
 	glutMainLoop();
