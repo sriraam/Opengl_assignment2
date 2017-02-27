d67ec923038c09dd0f66435a6efb2eeca01a5556
Sri Raam <Sri Raam@LAPTOP-JEB3P6D8>
2017-02-27 12:56:33 +0530

    polygon_added

--- opengl_as2/Source.cpp
+++ opengl_as2/Source.cpp
@@ -7,6 +7,7 @@ GLfloat v[4][2];
 GLint  obj[5];
 int static count=0;
 int mode = 1;
+int count_ver=0;
 int j = 0;
 bool handle=false;
 
@@ -69,17 +70,26 @@ public:
 void click_handle(int button, int state, int x, int y) {
 	if (button == GLUT_LEFT_BUTTON && state==GLUT_DOWN) {
 
-
+		
 		if (current_mode.size()==0) {
 			std::cout << "size =0";
 			current_mode.push_back(1);
 		}
-		
+		//For vertex point generation
 		v[count][0] = x;
 		v[count][1] = y;
 
+		//To keep track of ver
 		x_vec.push_back(x);
 		y_vec.push_back(y);
+
+		if (current_mode[current_mode.size()-1]==3) {
+			std::cout <<"countver"<< count_ver;
+			count_ver++;
+		}
+		else {
+			count_ver = 0;
+		}
 		
 		
 		handle = true;
@@ -92,9 +102,9 @@ void click_handle(int button, int state, int x, int y) {
 void init() {
 	glClearColor(1, 1, 1, 1);
 	gluOrtho2D(0, 640, 0, 480);
-	x_vec.reserve(10);
-	y_vec.reserve(10);
-	current_mode.reserve(5);
+	x_vec.reserve(20);
+	y_vec.reserve(20);
+	current_mode.reserve(10);
 	glFlush();
 }
 
@@ -105,7 +115,7 @@ void draw_line(int i_dp,int temp_dp) {
 		std::cout << "in line\n";
 		glBegin(GL_LINES);
 		//std::cout << "came to line";
-		glColor3f(0, 0, 0);
+		glColor3f(1, 0, 0);
 		//	glVertex2f(v[i][0], 480 - v[i][1]);
 		//  glVertex2f(v[i + 1][0], 480 - v[i + 1][1]);
 		glVertex2f(x_vec[i], 480 - y_vec[i]);
@@ -130,6 +140,19 @@ void draw_rect(int i_dp, int temp_dp) {
 	}
 }
 
+void draw_polygon(int i_dp,int temp_dp) {
+	int i = i_dp;
+	int temp = temp_dp;
+	glBegin(GL_LINE_LOOP);
+	std::cout << "entered polygon\n";
+	glColor3f(0, 0, 0);
+	for (i; i < count_ver;i++) {
+		glVertex2f(x_vec[i],480-y_vec[i]);
+		//glVertex2f(x_vec[i+1], 480 - y_vec[i+1]);
+	}
+	glEnd();
+}
+
 void render() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	if (handle == true) {
@@ -151,24 +174,29 @@ void render() {
 				i = 0;
 				
 				int temp;
-				//temp = count - (count % 2);
+				temp = count - (count % 2);
 
 				std::cout << "\n";
 				for (int j = 0; j < current_mode.size(); j++) {
 
 					if (current_mode[j] == 1) {
-						temp = count - (count % 2);
+					
 						std::cout << "j :" << j<<" val is"<< current_mode[j];
 						std::cout << "\nEntered line\n";
 						draw_line(i,temp);
 						i += 2;
 					}
 					else if (current_mode[j] == 2) {
-						temp = count - (count % 2);
+						
 						std::cout << "j :" << j << " val is" << current_mode[j];
 						std::cout << "\nEntered rect\n";
 						draw_rect(i, temp);
 						i += 2;
+					}
+					else if (current_mode[j]==3) {
+						draw_polygon(i, temp);
+						i += count_ver;
+						//count_ver = 0;
 					}
 				}
 				
